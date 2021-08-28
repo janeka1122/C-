e52733e85bc07d7d266e1506f417a96fd8bb78fd
janeka1122 <theloonybucket@gmail.com>
2021-08-28 19:42:28 +0530

    Project 1 - Hello World

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticeAcitivy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is my first practice activity
+            Console.WriteLine("Hello everyone");
+            Console.WriteLine("This is my first program");
+        }
+    }
+}
